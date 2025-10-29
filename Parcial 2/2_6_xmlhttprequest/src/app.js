bdaf31cc560a6d99afe0e8888b6b6ac766e9e5d1
RDanielParra <reyesparrareynaldodaniel@gmail.com>
2025-10-28 23:42:26 -0500

    ,

--- Parcial 2/2_6_xmlhttprequest/src/app.js
+++ Parcial 2/2_6_xmlhttprequest/src/app.js
@@ -1,6 +1,9 @@
 const btnCambiarXML = document.getElementById('btnCambiarXML')
 const btnCambiarFetch  = document.getElementById('btnCambiarFetch')
+const btnCambiarFetchAsync  = document.getElementById('btnCambiarFetchAsync')
+const btnCambiarJquery  = document.getElementById('btnCambiarJquery')
 const imgPokemon = document.getElementById('pokemon')
+import $ from 'jquery'
 
 
 let solicitud = new XMLHttpRequest()
@@ -24,5 +27,21 @@ btnCambiarFetch.addEventListener('click', async () => {
     .then(response => response.json())
     .then(res => {
         imgPokemon.setAttribute('src', res.sprites.front_default)
+    })
+})
+btnCambiarFetchAsync.addEventListener('click', async () => {
+    let random = Math.ceil(Math.random() * 100)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+    const payload = await response.json()
+    imgPokemon.setAttribute('src', payload.sprites.front_default)
+})
+btnCambiarJquery.addEventListener('click', () => {
+    let random = Math.ceil(Math.random() * 100)
+    $.ajax({
+        url: `https://pokeapi.co/api/v2/pokemon/${random}`,
+        method: 'POST',
+        success: (response) => {
+            console.log(response)
+        }
     })
 })
